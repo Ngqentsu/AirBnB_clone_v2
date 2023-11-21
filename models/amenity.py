da65fe1f528e0fb2f6cf69457d8e53e7615d91dd
Gilfoye-CentriX <125440612+Gilfoye-CentriX@users.noreply.github.com>
2023-11-21 12:15:44 +0200

    Models update

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
     name = ""
--- models/review.py
+++ models/review.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""This is the review class"""
+""" Review module for the HBNB project """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
+    """ Review classto store review information """
     place_id = ""
     user_id = ""
     text = ""
