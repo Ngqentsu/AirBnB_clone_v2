6787fa958022d044802cdf2e822e541c6faa65e9
Gilfoye-CentriX <125440612+Gilfoye-CentriX@users.noreply.github.com>
2023-11-21 12:13:40 +0200

    Models update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the amenity class"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
     name = ""
