7e636ac688beaed7292ea2118793b1d74dafe242
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-12-31 09:35:11 -0800

    A script that starts a Flask web application

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,7 +51,7 @@ def number_odd_or_even(n):
     else:
         number = 'odd'
     return render_template('6-number_odd_or_even.html', n=n,
-                           number =number)
+                           number=number)
 
 
 if __name__ == "__main__":
